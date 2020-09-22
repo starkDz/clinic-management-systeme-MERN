d8176100649a2ba18022e9006073b1fea5fd1b9b
Djehinet Fateh <fateh_djehinet@outlook.com>
2020-09-22 17:28:03 +0200

    22092020

--- client/src/modules/Dashboard/GlobaleStats.js
+++ client/src/modules/Dashboard/GlobaleStats.js
@@ -1,42 +1,42 @@
 import React, { useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Provider, connect } from 'react-redux';
+// import { Provider, connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import SaveIcon from '@material-ui/icons/Save';
+// import Typography from '@material-ui/core/Typography';
+// import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
+// import SaveIcon from '@material-ui/icons/Save';
 import axios from 'axios';
-import Cookies from 'universal-cookie';
+// import Cookies from 'universal-cookie';
 import { url } from '../../defaults/default';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // core components
 import DescriptionIcon from '@material-ui/icons/Description';
-import GridContainer from '../../components/Grid/GridContainer.js';
+// import GridContainer from '../../components/Grid/GridContainer.js';
 import GridItem from '../../components/Grid/GridItem.js';
-import Table from '../../components/Table/Table.js';
-import Danger from '../../components/Typography/Danger.js';
+// import Table from '../../components/Table/Table.js';
+// import Danger from '../../components/Typography/Danger.js';
 import Card from '../../components/Card/Card.js';
 import CardHeader from '../../components/Card/CardHeader.js';
 import CardIcon from '../../components/Card/CardIcon.js';
-import CardBody from '../../components/Card/CardBody.js';
+// import CardBody from '../../components/Card/CardBody.js';
 import CardFooter from '../../components/Card/CardFooter.js';
-import Store from '@material-ui/icons/Store';
+// import Store from '@material-ui/icons/Store';
 import BuildIcon from '@material-ui/icons/Build';
 import styles from '../../assets/jss/material-dashboard-pro-react/views/dashboardStyle.js';
-import Warning from '@material-ui/icons/Warning';
-import DateRange from '@material-ui/icons/DateRange';
-import LocalOffer from '@material-ui/icons/LocalOffer';
-import Update from '@material-ui/icons/Update';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import AccessTime from '@material-ui/icons/AccessTime';
-import Refresh from '@material-ui/icons/Refresh';
-import Edit from '@material-ui/icons/Edit';
-import Place from '@material-ui/icons/Place';
-import ArtTrack from '@material-ui/icons/ArtTrack';
-import Language from '@material-ui/icons/Language';
-import LaptopMacIcon from '@material-ui/icons/LaptopMac';
-import PersonIcon from '@material-ui/icons/Person';
+// import Warning from '@material-ui/icons/Warning';
+// import DateRange from '@material-ui/icons/DateRange';
+// import LocalOffer from '@material-ui/icons/LocalOffer';
+// import Update from '@material-ui/icons/Update';
+// import ArrowUpward from '@material-ui/icons/ArrowUpward';
+// import AccessTime from '@material-ui/icons/AccessTime';
+// import Refresh from '@material-ui/icons/Refresh';
+// import Edit from '@material-ui/icons/Edit';
+// import Place from '@material-ui/icons/Place';
+// import ArtTrack from '@material-ui/icons/ArtTrack';
+// import Language from '@material-ui/icons/Language';
+// import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+// import PersonIcon from '@material-ui/icons/Person';
 
 import { Icon, InlineIcon } from '@iconify/react';
 import iRegistration from '@iconify/icons-medical-icon/i-registration';
@@ -78,12 +78,7 @@ const Add_New = () => {
             <h1 className={classes.cardTitle}>{countData.NumberRendezVous}</h1>
           </CardHeader>
           <CardFooter stats>
-            <div className={classes.stats}>
-              <Danger>
-                <Warning />
-              </Danger>
-              Get more space
-            </div>
+            <div className={classes.stats}></div>
           </CardFooter>
         </Card>
       </GridItem>
@@ -97,10 +92,7 @@ const Add_New = () => {
             <h1 className={classes.cardTitle}>{countData.NumberPatient}</h1>
           </CardHeader>
           <CardFooter stats>
-            <div className={classes.stats}>
-              <DateRange />
-              Last 24 Hours
-            </div>
+            <div className={classes.stats}></div>
           </CardFooter>
         </Card>
       </GridItem>
@@ -116,14 +108,12 @@ const Add_New = () => {
             </h1>
           </CardHeader>
           <CardFooter stats>
-            <div className={classes.stats}>
-              <LocalOffer />
-              Tracked from Github
-            </div>
+            <div className={classes.stats}></div>
           </CardFooter>
         </Card>
       </GridItem>
     </Grid>
   );
 };
+
 export default Add_New;
